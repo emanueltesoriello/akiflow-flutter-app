77857fded826ab796380feb65e602703ac62dac2
Matteo Ricupero <matteo@akiflow.com>
2022-07-02 11:49:10 +0200

    feat: delete nlp text when tap from action

--- lib/extensions/date_extension.dart
+++ lib/extensions/date_extension.dart
@@ -1,7 +1,16 @@
+import 'package:intl/intl.dart';
+
 extension DateExtension on DateTime {
-    
-    int daysBetween(DateTime to) {
+  int daysBetween(DateTime to) {
     to = DateTime(to.year, to.month, to.day);
     return (to.difference(this).inHours / 24).round();
+  }
+
+  String get shortDateFormatted {
+    return DateFormat("d MMM").format(this);
+  }
+
+  String get timeFormatted {
+    return DateFormat("HH:mm").format(this);
   }
 }
