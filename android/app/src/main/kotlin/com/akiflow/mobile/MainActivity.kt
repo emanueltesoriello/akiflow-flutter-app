c6bd5215a4ed96b554ea119a92315baeac7eece9
joelle-supy-io <joelle@supy.io>
2022-11-17 10:13:39 +0200

    remove comment

--- android/app/src/main/kotlin/com/akiflow/mobile/MainActivity.kt
+++ android/app/src/main/kotlin/com/akiflow/mobile/MainActivity.kt
@@ -5,8 +5,6 @@ import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 class MainActivity: FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
-       ・・・
-
         if (intent.getIntExtra("org.chromium.chrome.extra.TASK_ID", -1) == this.taskId) {
             this.finish()
             intent.addFlags(FLAG_ACTIVITY_NEW_TASK);
