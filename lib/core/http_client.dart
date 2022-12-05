623faae2ad644389e8369fae43ee77614dbbc43f
joelle-supy-io <joelle@supy.io>
2022-12-05 22:18:42 +0200

    short lived access token

--- lib/core/http_client.dart
+++ lib/core/http_client.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:http/http.dart';
+import 'package:mobile/core/config.dart';
 import 'package:mobile/core/preferences.dart';
 import 'package:models/user.dart';
 
@@ -9,17 +13,66 @@ class HttpClient extends BaseClient {
 
   HttpClient(this._preferences) : _inner = Client();
 
+  Future<void> refreshToken(User? user) async {
+    if (user != null) {
+      Response response = await _inner.post(Uri.parse('https://app.akiflow.com/oauth/refreshToken'), body: {
+        "client_id": Config.oauthClientId,
+        "refresh_token": user.refreshToken,
+      });
+      Map<String, dynamic> map = jsonDecode(response.body);
+      await _preferences.saveUser(user.copyWith(accessToken: map["access_token"], refreshToken: map["refresh_token"]));
+    }
+  }
+
   @override
-  Future<StreamedResponse> send(BaseRequest request) {
+  Future<Response> get(Uri url, {Map<String, String>? headers}) async {
+    User? user = _preferences.user;
+    if (headers != null) {
+      headers.putIfAbsent(HttpHeaders.authorizationHeader, () => "Bearer ${user?.accessToken!}");
+    } else {
+      headers = {HttpHeaders.authorizationHeader: "Bearer ${user?.accessToken!}"};
+    }
+    Response response = await _inner.get(url, headers: headers);
+    if (response.statusCode == 401 || response.statusCode == 403) {
+      await refreshToken(user);
+      return _inner.get(url, headers: headers);
+    }
+    return response;
+  }
+
+  @override
+  Future<Response> post(Uri url, {Map<String, String>? headers, body, Encoding? encoding}) async {
+    User? user = _preferences.user;
+
+    if (headers != null) {
+      headers.putIfAbsent(HttpHeaders.authorizationHeader, () => "Bearer ${user?.accessToken!}");
+    } else {
+      headers = {HttpHeaders.authorizationHeader: "Bearer ${user?.accessToken!}"};
+    }
+    Response response = await _inner.post(url, headers: headers, body: body, encoding: encoding);
+
+    if (response.statusCode == 401 || response.statusCode == 403) {
+      await refreshToken(user);
+      return _inner.post(url, headers: headers, body: body, encoding: encoding);
+    }
+    return _inner.post(url, headers: headers, body: body, encoding: encoding);
+  }
+
+  @override
+  Future<StreamedResponse> send(BaseRequest request) async {
     User? user = _preferences.user;
 
     if (user != null) {
-      print(user.accessToken);
       request.headers['Authorization'] = "Bearer ${user.accessToken ?? ''}";
     }
 
     request.headers['Content-Type'] = "application/json";
 
-    return _inner.send(request);
+    StreamedResponse response = await _inner.send(request);
+    if (response.statusCode == 401 || response.statusCode == 403) {
+      await refreshToken(user);
+      return _inner.send(request);
+    }
+    return response;
   }
 }
