3587127788a9d5d9f0382979c3cbf191e6cfb577
joelle-supy-io <joelle@supy.io>
2022-12-07 14:07:54 +0200

    final fixes

--- lib/core/http_client.dart
+++ lib/core/http_client.dart
@@ -4,27 +4,14 @@ import 'package:mobile/core/config.dart';
 import 'package:mobile/core/preferences.dart';
 import 'package:models/user.dart';
 
+Uri refreshTokenUrl = Uri.parse('https://app.akiflow.com/oauth/refreshToken');
+
 class HttpClient extends BaseClient {
   final PreferencesRepository _preferences;
 
   final Client _inner;
 
   HttpClient(this._preferences) : _inner = Client();
-
-  Future<bool> refreshToken(User? user) async {
-    if (user != null) {
-      Response response = await _inner.post(Uri.parse('https://app.akiflow.com/oauth/refreshToken'), body: {
-        "client_id": Config.oauthClientId,
-        "refresh_token": user.refreshToken,
-      });
-      Map<String, dynamic> map = jsonDecode(response.body);
-      await _preferences.saveUser(user.copyWith(accessToken: map["access_token"], refreshToken: map["refresh_token"]));
-      return false;
-    }
-    await _preferences.clear();
-    return false;
-  }
-
   @override
   Future<StreamedResponse> send(BaseRequest request) async {
     User? user = _preferences.user;
@@ -46,5 +33,34 @@ class HttpClient extends BaseClient {
       }
     }
     return response;
+  }
+
+    Future<bool> refreshToken(User? user) async {
+    if (user != null) {
+      Response response = await _inner.post(refreshTokenUrl, body: {
+        "client_id": Config.oauthClientId,
+        "refresh_token": user.refreshToken,
+      });
+      Map<String, dynamic> map = jsonDecode(response.body);
+      if (map["access_token"] == null && response.statusCode.toString().startsWith('4')) {
+        return clear();
+      } else if (map["access_token"] != null) {
+        return updateTokens(user, map);
+      } else {
+        return refreshToken(user);
+      }
+    }
+    return clear();
+  }
+
+
+  Future<bool> clear() async {
+    await _preferences.clear();
+    return false;
+  }
+
+  Future<bool> updateTokens(User user, Map<String, dynamic> map) async {
+    await _preferences.saveUser(user.copyWith(accessToken: map["access_token"], refreshToken: map["refresh_token"]));
+    return true;
   }
 }
