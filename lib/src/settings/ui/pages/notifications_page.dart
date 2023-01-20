cc8a80b8b10345bbc6d39563fea1c841dd0f5a8f
Emanuel <emanuel.tesoriello@gmail.com>
2023-01-20 18:36:51 +0100

    Implemented base elements for the notifications settings

--- lib/src/settings/ui/pages/notifications_page.dart
+++ lib/src/settings/ui/pages/notifications_page.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:i18n/strings.g.dart';
 import 'package:mobile/common/style/colors.dart';
 import 'package:mobile/common/style/theme.dart';
+import 'package:mobile/common/utils/time_picker_utils.dart';
 import 'package:mobile/src/base/ui/widgets/base/app_bar.dart';
+import 'package:mobile/src/settings/ui/widgets/receive_notification_setting_modal.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 class NotificationsPage extends StatefulWidget {
   const NotificationsPage({Key? key}) : super(key: key);
@@ -12,7 +15,7 @@ class NotificationsPage extends StatefulWidget {
 }
 
 class _NotificationsPageState extends State<NotificationsPage> {
-  buttonElement() {
+  mainItem(String switchTitle, String mainButtonListTitle, String selectedButtonListItem, Function onTap) {
     return Container(
       margin: const EdgeInsets.all(1),
       //padding: const EdgeInsets.fromLTRB(8, 12, 8, 12),
@@ -25,58 +28,56 @@ class _NotificationsPageState extends State<NotificationsPage> {
       child: ListView(
         shrinkWrap: true,
         children: [
-          Expanded(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Flexible(
-                  child: SwitchListTile(
-                    value: true,
-                    onChanged: (newVal) {},
-                    activeColor: ColorsExt.akiflow(context),
-                    title: Text(
-                      "New task",
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                        fontSize: 17,
-                        color: ColorsExt.grey2(context),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
+          SwitchListTile(
+            value: true,
+            onChanged: (newVal) {},
+            activeColor: ColorsExt.akiflow(context),
+            title: Text(
+              switchTitle,
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                fontSize: 17,
+                color: ColorsExt.grey2(context),
+              ),
             ),
           ),
           const Divider(),
-          Expanded(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Flexible(
-                  child: ListTile(
-                    title: Text(
-                      "Receive notifications",
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                        color: ColorsExt.grey2(context),
-                      ),
-                    ),
-                    subtitle: Text(
-                      "Receive notifications",
-                      textAlign: TextAlign.left,
-                      style: Theme.of(context)
-                          .textTheme
-                          .subtitle2
-                          ?.copyWith(color: ColorsExt.grey3(context), fontWeight: FontWeight.normal),
-                    ),
-                    trailing: const Icon(Icons.arrow_forward_ios),
-                  ),
-                ),
-              ],
+          ListTile(
+            onTap: () => onTap(),
+            title: Text(
+              mainButtonListTitle,
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                fontSize: 17,
+                color: ColorsExt.grey2(context),
+              ),
             ),
+            subtitle: Text(
+              selectedButtonListItem,
+              textAlign: TextAlign.left,
+              style: Theme.of(context)
+                  .textTheme
+                  .subtitle2
+                  ?.copyWith(color: ColorsExt.grey3(context), fontWeight: FontWeight.normal),
+            ),
+            trailing: const Icon(Icons.arrow_forward_ios),
           ),
         ],
       ),
+    );
+  }
+
+  onReceiveNotificationNextTaskClick() async {
+    await showCupertinoModalBottomSheet(
+        context: context, builder: (context) => const ReceiveNotificationSettingModal());
+  }
+
+  onReceiveNotificationDailyOverviewClick() {
+    TimeOfDay initialTime = const TimeOfDay(hour: 8, minute: 0);
+    TimePickerUtils.pick(
+      context,
+      initialTime: initialTime,
+      onTimeSelected: (selected) {},
     );
   }
 
@@ -100,13 +101,16 @@ class _NotificationsPageState extends State<NotificationsPage> {
                   style: TextStyle(fontSize: 11, fontWeight: FontWeight.w500, color: ColorsExt.grey3(context)),
                 ),
                 const SizedBox(height: 5),
-                buttonElement(),
+                mainItem("Next tasks", "Receive notification", "X minutes before the task starts",
+                    () => onReceiveNotificationNextTaskClick()),
                 const SizedBox(height: 20),
                 Text(
                   "DAILY OVERVIEW".toUpperCase(),
                   style: TextStyle(fontSize: 11, fontWeight: FontWeight.w500, color: ColorsExt.grey3(context)),
                 ),
-                buttonElement(),
+                const SizedBox(height: 5),
+                mainItem("Daily overview notification", "Receive notification", "At 10:00",
+                    () => onReceiveNotificationDailyOverviewClick()),
               ],
             ),
           ),
---
+++ lib/src/settings/ui/widgets/receive_notification_setting_modal.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/common/style/colors.dart';
+import 'package:mobile/src/base/ui/widgets/base/scroll_chip.dart';
+
+class ReceiveNotificationSettingModal extends StatefulWidget {
+  const ReceiveNotificationSettingModal({Key? key}) : super(key: key);
+
+  @override
+  State<ReceiveNotificationSettingModal> createState() => _ReceiveNotificationSettingModalState();
+}
+
+class _ReceiveNotificationSettingModalState extends State<ReceiveNotificationSettingModal> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        color: Theme.of(context).backgroundColor,
+        child: AnimatedSize(
+          curve: Curves.elasticOut,
+          duration: const Duration(milliseconds: 400),
+          child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.transparent,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(16.0),
+                  topRight: Radius.circular(16.0),
+                ),
+              ),
+              margin: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+              child: ListView(
+                shrinkWrap: true,
+                children: [
+                  const SizedBox(height: 12),
+                  const ScrollChip(),
+                  const SizedBox(height: 12),
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Send notifications...',
+                            style: Theme.of(context).textTheme.titleLarge?.copyWith(color: ColorsExt.grey2(context))),
+                        const SizedBox(height: 12),
+                        ...List.generate(
+                          5,
+                          (index) => Container(
+                            padding: const EdgeInsets.symmetric(vertical: 15),
+                            child: Text('${index * 5} minutes before',
+                                style: Theme.of(context).textTheme.bodyText2?.copyWith(
+                                      fontSize: 17,
+                                      color: ColorsExt.grey2(context),
+                                    )),
+                          ),
+                        ).reversed,
+                        const SizedBox(height: 50),
+                      ],
+                    ),
+                  ),
+                ],
+              )),
+        ));
+  }
+}
