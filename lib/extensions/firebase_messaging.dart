ecfe93d6bbbde9becf3c4884cd2261870c8bbcee
Emanuel <emanuel.tesoriello@gmail.com>
2023-02-22 10:49:54 +0100

    changed remote notification type name

--- lib/extensions/firebase_messaging.dart
+++ lib/extensions/firebase_messaging.dart
@@ -10,7 +10,7 @@ onNotificationsReceived(RemoteMessage message, FlutterLocalNotificationsPlugin f
   String notificationType = message.data["notification_type"] ?? '';
   // If `onMessage` is triggered with a notification, construct our own
   // local notification to show to users using the created channel.
-  if (notificationType == 'trigger_sync') {
+  if (notificationType == 'trigger_sync:tasks,events,whatever_we_will_add') {
     backgroundProcesses(backgroundSyncFromNotification);
   }
   //TODO add support for other  type of notifications like the handling of the visible ones
