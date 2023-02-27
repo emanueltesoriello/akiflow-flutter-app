c26ffa611b619e6bf387fc3715fa4c3164f7d640
Emanuel <emanuel.tesoriello@gmail.com>
2023-02-27 19:36:52 +0100

    improved background services

--- lib/core/services/background_service.dart
+++ lib/core/services/background_service.dart
@@ -12,6 +12,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:workmanager/workmanager.dart';
 import 'package:workmanager/src/options.dart' as constraints;
 import 'package:mobile/core/preferences.dart';
+import 'package:mobile/core/preferences.dart';
 
 const scheduleNotificationsTaskKey = "com.akiflow.mobile.scheduleNotifications";
 const periodicTaskskKey = "com.akiflow.mobile.periodicTask";
@@ -20,61 +21,56 @@ const backgroundSyncFromNotification = "com.akiflow.mobile.backgroundSyncFromNot
 @pragma('vm:entry-point')
 void callbackDispatcher() {
   Workmanager().executeTask((task, inputData) async {
-    WidgetsFlutterBinding.ensureInitialized();
-    AppLifecycleState? state = WidgetsBinding.instance.lifecycleState;
+    // listen on this port in order to catch trigger from the background services.
+    // Useful for UI updates based on background sync
+    final sendPort = IsolateNameServer.lookupPortByName("backgroundSync");
 
-    if (state == AppLifecycleState.resumed) {
-      print("the app is in foreground. No background process will be executed.");
-
-      return Future.value(true);
-    } else {
-      // listen on this port in order to catch trigger from the background services.
-      // Useful for UI updates based on background sync
-      final sendPort = IsolateNameServer.lookupPortByName("backgroundSync");
-
-      if (sendPort != null) {
-        // N.B. The port might be null if the main isolate is not running.
-        sendPort.send(['backgroundSync']); //change this in order to send datas to all the listeners.
-      }
-
-      // *********************************************
-      // ***** init services *************************
-      // *********************************************
-      SharedPreferences preferences = await SharedPreferences.getInstance();
-      DatabaseService databaseService = DatabaseService();
-      if (databaseService.database == null || !databaseService.database!.isOpen) {
-        await databaseService.open(skipDirectoryCreation: true);
-        print('new database opened - backgroundProcesses');
-      } else {
-        print('database already opened - backgroundProcesses');
-      }
-      await Config.initialize(
-        configFile: 'assets/config/prod.json',
-        production: true,
-      );
-      try {
-        setupLocator(preferences: preferences, databaseService: databaseService, initFirebaseApp: false);
-      } on ArgumentError catch (e, _) {
-        if (e.message.toString().contains('type HttpClient is already registered')) {
-          print('Locator already initialized!');
-        }
-      } catch (e) {
-        print(e);
-      }
-
-      return backgroundProcesses(task);
+    if (sendPort != null) {
+      // N.B. The port might be null if the main isolate is not running.
+      sendPort.send(['backgroundSync']); //change this in order to send datas to all the listeners.
     }
+
+    // *********************************************
+    // ***** init services *************************
+    // *********************************************
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    DatabaseService databaseService = DatabaseService();
+    if (databaseService.database == null || !databaseService.database!.isOpen) {
+      await databaseService.open(skipDirectoryCreation: true);
+      print('new database opened - backgroundProcesses');
+    } else {
+      print('database already opened - backgroundProcesses');
+    }
+    await Config.initialize(
+      configFile: 'assets/config/prod.json',
+      production: true,
+    );
+    try {
+      setupLocator(preferences: preferences, databaseService: databaseService, initFirebaseApp: false);
+    } on ArgumentError catch (e, _) {
+      if (e.message.toString().contains('type HttpClient is already registered')) {
+        print('Locator already initialized!');
+      }
+    } catch (e) {
+      print(e);
+    }
+    return backgroundProcesses(task, db: databaseService);
   });
 }
 
-Future<bool> backgroundProcesses(String task) async {
+Future<bool> backgroundProcesses(String task, {DatabaseService? db}) async {
   try {
     // *********************************************
     // ***** background notifications scheduling ***
     // *********************************************
     final SyncControllerService syncControllerService = locator<SyncControllerService>();
-
+    // if (DateTime.now().toUtc().difference(locator<PreferencesRepository>().lastTasksSyncAt!).inMinutes > 15 ||
+    //   task != backgroundSyncFromNotification) {
     await syncControllerService.sync();
+    if (db != null) {
+      db.database!.close();
+    }
+    // }
 
     // Show a local notification to confirm the background Sync
     if (kDebugMode) {
@@ -111,7 +107,7 @@ class BackgroundService {
     Workmanager().registerPeriodicTask(
       periodicTaskskKey,
       periodicTaskskKey,
-      initialDelay: const Duration(minutes: 10),
+      initialDelay: const Duration(minutes: 15),
       constraints: constraints.Constraints(
         // connected or metered mark the task as requiring internet
         networkType: NetworkType.connected,
