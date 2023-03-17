e9a1b017ecaf94c555559bcfe95f5bc8db99ed30
Victor <victor@akiflow.com>
2023-03-17 12:00:09 +0200

    today button is colored only when visible dates don't contain current day

--- lib/src/calendar/ui/widgets/calendar_appbar.dart
+++ lib/src/calendar/ui/widgets/calendar_appbar.dart
@@ -22,91 +22,92 @@ class CalendarAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AppBarComp(
-      shadow: false,
-      titleWidget: _buildTitle(context, calendarController),
-      leading: SvgPicture.asset(
-        Assets.images.icons.common.calendarSVG,
-        width: 26,
-        height: 26,
-      ),
-      actions: [
-        TextButton(
-          onPressed: () {
-            DateTime now = DateTime.now().toLocal();
-            calendarController.displayDate = now.hour > 2 ? now.subtract(const Duration(hours: 2)) : now;
-            context.read<CalendarCubit>().closePanel();
-          },
-          style: ButtonStyle(
-            overlayColor: MaterialStateProperty.all<Color>(Colors.transparent),
+    DateTime now = DateTime.now().toLocal();
+    DateTime today = DateTime(now.year, now.month, now.day);
+    return BlocBuilder<CalendarCubit, CalendarCubitState>(
+      builder: (context, state) {
+        return AppBarComp(
+          shadow: false,
+          titleWidget: _buildTitle(context, state, calendarController),
+          leading: SvgPicture.asset(
+            Assets.images.icons.common.calendarSVG,
+            width: 26,
+            height: 26,
           ),
-          child: Text(
-            t.bottomBar.today,
-            style: TextStyle(
-              fontSize: 15,
-              fontWeight: FontWeight.w600,
-              color: ColorsExt.akiflow(context),
+          actions: [
+            TextButton(
+              onPressed: () {
+                calendarController.displayDate = now.hour > 2 ? now.subtract(const Duration(hours: 2)) : now;
+                context.read<CalendarCubit>().closePanel();
+              },
+              style: ButtonStyle(
+                overlayColor: MaterialStateProperty.all<Color>(Colors.transparent),
+              ),
+              child: Text(
+                t.bottomBar.today,
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w600,
+                  color: state.visibleDates.contains(today) ? ColorsExt.grey3(context) : ColorsExt.akiflow(context),
+                ),
+              ),
             ),
-          ),
-        ),
-        IconButton(
-          icon: SvgPicture.asset(
-            Assets.images.icons.common.ellipsisSVG,
-            width: 24,
-            height: 24,
-            color: ColorsExt.grey3(context),
-          ),
-          onPressed: () {
-            showCupertinoModalBottomSheet(
-              context: context,
-              builder: (context) => CalendarSettingsModal(calendarController: calendarController),
-            );
-            context.read<CalendarCubit>().closePanel();
-          },
-        )
-      ],
-      showSyncButton: true,
+            IconButton(
+              icon: SvgPicture.asset(
+                Assets.images.icons.common.ellipsisSVG,
+                width: 24,
+                height: 24,
+                color: ColorsExt.grey3(context),
+              ),
+              onPressed: () {
+                showCupertinoModalBottomSheet(
+                  context: context,
+                  builder: (context) => CalendarSettingsModal(calendarController: calendarController),
+                );
+                context.read<CalendarCubit>().closePanel();
+              },
+            )
+          ],
+          showSyncButton: true,
+        );
+      },
     );
   }
 
   @override
   Size get preferredSize => const Size.fromHeight(56);
 
-  Widget _buildTitle(BuildContext context, CalendarController calendarController) {
+  Widget _buildTitle(BuildContext context, CalendarCubitState state, CalendarController calendarController) {
     return InkWell(
       overlayColor: MaterialStateProperty.all<Color>(Colors.transparent),
       onTap: () {
         context.read<CalendarCubit>().tapAppBarTextDate();
       },
-      child: BlocBuilder<CalendarCubit, CalendarCubitState>(
-        builder: (context, state) {
-          return Row(
-            children: [
-              Text(
-                DateFormat('MMMM').format(state.visibleDates.isEmpty
-                    ? DateTime.now().toLocal()
-                    : state.visibleDates.length > 29
-                        ? state.visibleDates.elementAt(7)
-                        : state.visibleDates.first),
-                textAlign: TextAlign.start,
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 22, color: ColorsExt.grey2(context)),
-              ),
-              const SizedBox(width: 10),
-              SizedBox(
-                width: 16,
-                height: 16,
-                child: SvgPicture.asset(
-                  state.panelState == PanelState.closed
-                      ? Assets.images.icons.common.chevronDownSVG
-                      : Assets.images.icons.common.chevronUpSVG,
-                  color: ColorsExt.grey3(context),
-                ),
-              ),
-            ],
-          );
-        },
+      child: Row(
+        children: [
+          Text(
+            DateFormat('MMMM').format(state.visibleDates.isEmpty
+                ? DateTime.now().toLocal()
+                : state.visibleDates.length > 29
+                    ? state.visibleDates.elementAt(7)
+                    : state.visibleDates.first),
+            textAlign: TextAlign.start,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(fontWeight: FontWeight.w500, fontSize: 22, color: ColorsExt.grey2(context)),
+          ),
+          const SizedBox(width: 10),
+          SizedBox(
+            width: 16,
+            height: 16,
+            child: SvgPicture.asset(
+              state.panelState == PanelState.closed
+                  ? Assets.images.icons.common.chevronDownSVG
+                  : Assets.images.icons.common.chevronUpSVG,
+              color: ColorsExt.grey3(context),
+            ),
+          ),
+        ],
       ),
     );
   }
