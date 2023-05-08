4ce79235964205e7280ed28df2c010afae68a269
Victor <victor@akiflow.com>
2023-05-08 17:41:31 +0300

    refactor: label in task modal

--- lib/src/tasks/ui/pages/edit_task/edit_task_row.dart
+++ lib/src/tasks/ui/pages/edit_task/edit_task_row.dart
@@ -249,8 +249,9 @@ class _EditTaskRowState extends State<EditTaskRow> {
     }
 
     return TagBox(
+      isBig: true,
       icon: Assets.images.icons.common.numberSVG,
-      text: label?.title ?? t.editTask.noLabel,
+      text: label?.title ?? t.editTask.addLabel,
       backgroundColor:
           label?.color != null ? ColorsExt.getFromName(label!.color!).withOpacity(0.1) : ColorsExt.grey100(context),
       iconColor: label?.color != null ? ColorsExt.getFromName(label!.color!) : ColorsExt.grey600(context),
