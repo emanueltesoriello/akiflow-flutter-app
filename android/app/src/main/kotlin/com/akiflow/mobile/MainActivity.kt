9e823d440e95bdc3a7ed9012c2dbdce16a1ae33b
Victor <victor@akiflow.com>
2023-06-09 18:10:51 +0300

    android and iOS native methods to return system first day of week

--- android/app/src/main/kotlin/com/akiflow/mobile/MainActivity.kt
+++ android/app/src/main/kotlin/com/akiflow/mobile/MainActivity.kt
@@ -1,6 +1,23 @@
 package com.akiflow.mobile
 
+import android.os.Bundle
+
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.akiflow.mobile/firstDayOfWeek"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "getFirstDayOfWeek") {
+                val firstDayOfWeek = FirstDayOfWeekUtil.getFirstDayOfWeek(applicationContext)
+                result.success(firstDayOfWeek)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
 }
